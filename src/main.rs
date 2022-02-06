f9fe8b5ffa0a8069e924e9dbada1e5e28e16fd19
nissy-dev <nd.12021218@gmail.com>
2022-02-06 23:55:28 +0900

    refactor(dioxus): come back dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,11 @@
-use tokio::task::spawn_blocking;
-use tokio::task::LocalSet;
-use warp::Filter;
-use yew::prelude::*;
+use dioxus::prelude::*;
 
-#[function_component]
-fn App() -> Html {
-    let fallback = html! {<div>{"Loading..."}</div>};
-
-    html! {
-        <Suspense {fallback}>
-            <Content />
-        </Suspense>
-    }
+fn main() {
+    dioxus::web::launch(App);
 }
 
-async fn render() -> String {
-    let content = spawn_blocking(move || {
-        use tokio::runtime::Builder;
-        let set = LocalSet::new();
-
-        let rt = Builder::new_current_thread().enable_all().build().unwrap();
-
-        set.block_on(&rt, async {
-            let renderer = yew::ServerRenderer::<App>::new();
-
-            renderer.render().await
-        })
-    })
-    .await
-    .expect("the thread has failed.");
-
-    format!(
-        r#"<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>Yew SSR Example</title>
-    </head>
-    <body>
-        {}
-    </body>
-</html>
-"#,
-        content
-    )
-}
-
-#[tokio::main]
-async fn main() {
-    let routes = warp::any().then(|| async move { warp::reply::html(render().await) });
-
-    println!("You can view the website at: http://localhost:8080/");
-
-    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
+fn App(cx: Scope) -> Element {
+    cx.render(rsx!(
+        h1 { "hello world" }
+    ))
 }
