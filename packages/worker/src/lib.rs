bdaef45ed6de7d3640d337fe9c61eb22e6dc3c3a
nissy-dev <nd.12021218@gmail.com>
2022-02-23 23:28:28 +0900

    feat(worker): worker return dioxus ssr results

--- packages/worker/src/lib.rs
+++ packages/worker/src/lib.rs
@@ -1,4 +1,5 @@
-use serde_json::json;
+use app;
+use dioxus::prelude::*;
 use worker::*;
 
 mod utils;
@@ -17,39 +18,16 @@ fn log_request(req: &Request) {
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
     log_request(&req);
 
-    // Optionally, get more helpful error messages written to the console in the case of a panic.
+    // Get more helpful error messages written to the console in the case of a panic.
     utils::set_panic_hook();
 
-    // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
-    // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
-    // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
     let router = Router::new();
+    router.get("/", handle_request).run(req, env).await
+}
 
-    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
-    // functionality and a `RouteContext` which you can use to  and get route parameters and
-    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
-    router
-        .get("/", |_, _| Response::ok("Hello from Workers!"))
-        .post_async("/form/:field", |mut req, ctx| async move {
-            if let Some(name) = ctx.param("field") {
-                let form = req.form_data().await?;
-                match form.get(name) {
-                    Some(FormEntry::Field(value)) => {
-                        return Response::from_json(&json!({ name: value }))
-                    }
-                    Some(FormEntry::File(_)) => {
-                        return Response::error("`field` param in form shouldn't be a File", 422);
-                    }
-                    None => return Response::error("Bad Request", 400),
-                }
-            }
-
-            Response::error("Bad Request", 400)
-        })
-        .get("/worker-version", |_, ctx| {
-            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
-            Response::ok(version)
-        })
-        .run(req, env)
-        .await
+fn handle_request<D>(_req: Request, _ctx: RouteContext<D>) -> Result<Response> {
+    let mut vdom = VirtualDom::new(app::app);
+    let _ = vdom.rebuild();
+    println!("{}", dioxus::ssr::render_vdom(&vdom));
+    Response::from_html(dioxus::ssr::render_vdom(&vdom))
 }
